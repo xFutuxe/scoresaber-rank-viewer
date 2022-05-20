8deb085b7faafd263ed452975fec64ce8f51666a
xFutuxe <baileysteadman@protonmail.com>
2022-05-21 00:33:15 +0100

    Odometer animations working
    Finally got odometer animations working for numbers.. All because I didn't call the animation
    
    (This won't appear if you download this and use it at this stage since the backend is not present on the repo)

--- website/js/script.js
+++ website/js/script.js
@@ -1,18 +1,27 @@
+
+
 function scoreSaber() {
     //  Fetches value from input box
     var scoreSaberId = document.getElementById("scoreSaberID").value
 
 
-    url = fetch('https://new.scoresaber.com/api/player/'+scoreSaberId+'/full')
+    url = fetch('https://scoresaber.com/api/player/'+scoreSaberId+'/full')
   .then(function (response) {
     return response.json();
   })
   .then(function (myJson) {
       // Collect Data from ScoreSaber API with submitted ID
-    console.log(myJson.playerInfo.playerName); // Player Name
-    document.getElementById("name").innerHTML = myJson.playerInfo.playerName
-
-
+    console.log(myJson.name); // Player Name
+    document.getElementById("name").innerHTML = myJson.name
+    var globalRank = myJson.rank
+    var countryRank = myJson.countryRank
+    setTimeout(function(){
+      document.getElementById("globalRank").innerHTML = globalRank;
+    }, 100);
+    setTimeout(function(){
+      document.getElementById("countryRank").innerHTML = countryRank;
+    }, 100);
+    
   })
   .catch(function (error) {
     console.log("Error: " + error);

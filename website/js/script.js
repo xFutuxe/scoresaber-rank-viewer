dd871d2a55cb941848ff901e3e31000171d58ebc
xFutuxe <baileysteadman@protonmail.com>
2022-05-24 15:04:10 +0100

    update

--- website/js/script.js
+++ website/js/script.js
@@ -4,7 +4,7 @@ function scoreSaber() {
     //  Fetches value from input box
     var scoreSaberId = document.getElementById("scoreSaberID").value
 
-
+  // Fetches API from url
     url = fetch('https://scoresaber.com/api/player/'+scoreSaberId+'/full')
   .then(function (response) {
     return response.json();
@@ -15,11 +15,29 @@ function scoreSaber() {
     document.getElementById("name").innerHTML = myJson.name
     var globalRank = myJson.rank
     var countryRank = myJson.countryRank
+    var pp = myJson.pp
+    var playerCountry = myJson.country
+    var totalScore = myJson.totalScore
+    var totalRankedScore = myJson.scoreStats.totalRankedScore
+    var accuracy = myJson.scoreStats.averageRankedAccuracy
+    var profilePicture = myJson.profilePicture
+    document.getElementById("profilePicture").src = profilePicture
+
+    // setTimeout to declare animation for odometer 
     setTimeout(function(){
       document.getElementById("globalRank").innerHTML = globalRank;
     }, 100);
     setTimeout(function(){
       document.getElementById("countryRank").innerHTML = countryRank;
+    }, 100);
+    setTimeout(function(){
+      document.getElementById("playerPP").innerHTML = pp;
+    }, 100);
+    setTimeout(function(){
+      document.getElementById("playerTotalScore").innerHTML = totalScore;
+    }, 100);
+    setTimeout(function(){
+      document.getElementById("playerTotalRankedScore").innerHTML = totalRankedScore;
     }, 100);
     
   })

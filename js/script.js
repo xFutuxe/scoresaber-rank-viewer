08d90e20493807626606d77f7fbbfb1f49abf9d0
xFutuxe <65718499+xFutuxe@users.noreply.github.com>
2022-05-30 19:37:56 +0100

    New mobile alerts added

--- js/script.js
+++ js/script.js
@@ -21,6 +21,13 @@ function resize_to_fit() {
 
 $(document).ready(function() {
 
+   var screenSize = window.matchMedia("(max-width: 500px)")
+   function checkScreenSize(screenSize) {
+     if (screenSize.matches) {
+       alert("Mobile does not work properly. For a better experience please vist on desktop.");
+     }
+   }
+   checkScreenSize();
     scoreSaberOnLoad();
     // Allows you to press enter instead of using the submit button
     $('input').keyup(function(event) {

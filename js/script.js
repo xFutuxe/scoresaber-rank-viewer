e4aacb2a8d770155d5df8fa675ddd7839b0204dc
xFutuxe <baileysteadman@protonmail.com>
2022-05-31 20:19:07 +0100

    New animations

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-
-// Font resizes depending on text length (Not really effecient as it doesn't scale up font but its fine for now)
-function resize_to_fit2() {
-    var fontsize = $('#name').css('font-size');
-    $('#name').css('fontSize', parseFloat(fontsize) - 1);
-  
-    if ($('#name').height() >= 60) {
-      resize_to_fit2();
-    }
-  }
-  
-  function resize_to_fit() {
-    var fontsize = $('#playerTotalRankedScore').css('font-size');
-    $('#playerTotalRankedScore').css('fontSize', parseFloat(fontsize) - 1);
-  
-    if ($('#playerTotalRankedScore').height() >= 30) {
-      resize_to_fit();
-    }
-  
-  }
-  
   $(document).ready(function() {
-  
+
+    var url = 'https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/players/count';
+    var storedText;
+    
+    fetch(url)
+      .then(function(response) {
+        response.text().then(function(text) {
+          storedText = text;
+          document.getElementById("playerCount").innerHTML = storedText
+        });
+      });
+    
+
       scoreSaberOnLoad();
       // Allows you to press enter instead of using the submit button
       $('input').keyup(function(event) {
@@ -52,7 +43,7 @@ function resize_to_fit2() {
         // Collect Data from ScoreSaber API with submitted ID
       console.log(myJson.name); // Player Name
       document.getElementById("name").innerHTML = myJson.name + " | " + myJson.country
-      resize_to_fit2();
+      document.getElementById("profileLink").href = "https://www.scoresaber.com/u/" + scoresaberIDs[random]
       var globalRank = myJson.rank
       var countryRank = myJson.countryRank
       var pp = myJson.pp
@@ -77,7 +68,6 @@ function resize_to_fit2() {
       setTimeout(function(){
         document.getElementById("playerTotalRankedScore").innerHTML = totalRankedScore;
       }, 100);
-      resize_to_fit();
     })
     .catch(function (error) {
       console.log("Error: " + error);
@@ -101,7 +91,7 @@ function resize_to_fit2() {
         // Collect Data from ScoreSaber API with submitted ID
       console.log(myJson.name); // Player Name
       document.getElementById("name").innerHTML = myJson.name + " | " + myJson.country
-      resize_to_fit2();
+      document.getElementById("profileLink").href = "https://www.scoresaber.com/u/" + scoreSaberId
       var globalRank = myJson.rank
       var countryRank = myJson.countryRank
       var pp = myJson.pp
@@ -125,7 +115,6 @@ function resize_to_fit2() {
       setTimeout(function(){
         document.getElementById("playerTotalRankedScore").innerHTML = totalRankedScore;
       }, 100);
-      resize_to_fit();
     })
     .catch(function (error) {
       console.log("Error: " + error);

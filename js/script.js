f70f7ffc430b8c48092535bc52edd94155ae068b
xFutuxe <65718499+xFutuxe@users.noreply.github.com>
2022-09-23 21:41:10 +0100

    see changes in changelog.txt

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 
-  let url = 'https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/players/count';
+  const url = 'https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/players/count';
   let storedText;
 
   fetch(url)
     .then(function (response) {
       response.text().then(function (text) {
-        storedText = text;
-        document.getElementById("playerCount").innerHTML = storedText
+        storedText = Number(text).toLocaleString();
+        document.getElementById("playerCount").innerHTML = storedText + " players on Score Saber"
       });
     });
 
@@ -24,37 +24,43 @@ $(document).ready(function () {
   //On site load ends below here
 });
 
-// Picks a random ID from array
-let scoresaberIDs = ['76561198333869741', '76561198045386379', '76561198308438667', '76561198309885407', '2769016623220259', '76561198205370764', '76561198180044686']
-const random = Math.floor(Math.random() * scoresaberIDs.length);
-console.log(scoresaberIDs[random]);
+
 
 // Functions calls when Submit button is pressed on site
 function scoreSaberOnLoad() {
 
+  function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+  const rndInt = randomIntFromInterval(1, 100)
+  console.log(rndInt)
 
   // Fetches API from url
-  url = fetch('https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/player/' + scoresaberIDs[random] + '/full')
+  url = fetch('https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/players?page='+rndInt+'&withMetadata=true')
     .then(function (response) {
       return response.json();
     })
     .then(function (myJson) {
-      // Collect Data from ScoreSaber API with submitted ID
-      console.log(myJson.name); // Player Name
-      document.getElementById("name").innerHTML = myJson.name + " | " + myJson.country
-      document.getElementById("profileLink").href = "https://www.scoresaber.com/u/" + scoresaberIDs[random]
-      let globalRank = myJson.rank
-      let countryRank = myJson.countryRank
-      let pp = myJson.pp
-      let roundedPP = Math.trunc(pp)
-      let playerCountry = myJson.country
-      let totalScore = myJson.totalScore
-      let totalRankedScore = myJson.scoreStats.totalRankedScore
-      let accuracy = myJson.scoreStats.averageRankedAccuracy
-      let profilePicture = myJson.profilePicture
-      document.getElementById("profilePicture").src = profilePicture
 
-      // setTimeout to declare animation for odometer 
+      function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+      
+      const rndInt = randomIntFromInterval(0, 50)
+      console.log(rndInt)
+
+      console.log(myJson.players[rndInt].id)
+      console.log(myJson.players[rndInt].rank)
+      let globalRank = myJson.players[rndInt].rank
+      let countryRank = myJson.players[rndInt].countryRank
+      let pp = myJson.players[rndInt].pp
+      let totalScore = myJson.players[rndInt].scoreStats.totalScore
+      let totalRankedScore = myJson.players[rndInt].scoreStats.totalRankedScore
+      let profilePicture = myJson.players[rndInt].profilePicture
+      document.getElementById("profilePicture").src = profilePicture
+      document.getElementById("name").innerHTML = myJson.players[rndInt].name + " | " + myJson.players[rndInt].country
+      document.getElementById("profileLink").href = "https://www.scoresaber.com/u/" + myJson.players[rndInt].id
       setTimeout(function () {
         document.getElementById("globalRank").innerHTML = globalRank;
       }, 100);
@@ -62,17 +68,17 @@ function scoreSaberOnLoad() {
         document.getElementById("countryRank").innerHTML = countryRank;
       }, 100);
       setTimeout(function () {
-        document.getElementById("playerPP").innerHTML = roundedPP;
+        document.getElementById("playerPP").innerHTML = pp;
       }, 100);
       setTimeout(function () {
         document.getElementById("playerTotalRankedScore").innerHTML = totalRankedScore;
       }, 100);
+
     })
     .catch(function (error) {
-      console.log("Error: " + error);
+      console.log(`Error: ${error}`);
     });
 }
-
 
 
 // Functions calls when Submit button is pressed on site

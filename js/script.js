3035c688bd345adf217e2bc84a4f6a127e14730a
Bailey S <65718499+xFutuxe@users.noreply.github.com>
2023-10-25 16:17:16 +0100

    Update script.js
    Fixed CORS issue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  const url = 'https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/players/count';
+  const url = 'https://corsproxy.io/https://scoresaber.com/api/players/count';
   let storedText;
 
   fetch(url)
@@ -36,7 +36,7 @@ function scoreSaberOnLoad() {
   console.log(rndInt)
 
   // Fetches API from url
-  url = fetch('https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/players?page='+rndInt+'&withMetadata=true')
+  url = fetch('https://corsproxy.io/https://scoresaber.com/api/players?page='+rndInt+'&withMetadata=true')
     .then(function (response) {
       return response.json();
     })
@@ -100,7 +100,7 @@ function scoreSaber() {
   } else {
 
      // Fetches API from url
-  url = fetch('https://radiant-dawn-45124.herokuapp.com/https://scoresaber.com/api/player/' + scoreSaberId + '/full')
+  url = fetch('https://corsproxy.io/https://scoresaber.com/api/player/' + scoreSaberId + '/full')
   .then(function (response) {
     return response.json();
   })
